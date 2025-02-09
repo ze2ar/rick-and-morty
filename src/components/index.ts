72d1ef8838fb53d1285a5f7377fac0a4103cb755
Arseniy Zelenichenko <117036995+ze2ar@users.noreply.github.com>
2025-02-09 22:56:42 +0500

    feat: Add spinner component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as CharacterPreviewCard } from './character-preview-card';
 export { default as CharacterEpisodes } from './character-episodes';
+export { default as LoadingSpinner } from './loading-spinner';
---
+++ src/components/loading-spinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoadingSpinner(): React.ReactNode {
+  return (
+    <div className='loading-spinner'>
+      <div className='spinner'></div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
